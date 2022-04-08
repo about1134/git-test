79609e1577565dd4917627bbb3eb2c724016a10f
arbout <about1134@petalmail.com>
2022-04-08 16:10:32 +0800

    hot-fix commit

--- src/main/java/com/ck/GitTest.java
+++ src/main/java/com/ck/GitTest.java
@@ -3,11 +3,13 @@ package com.ck;
 /**
  * @author about1134
  * @create 2022-04-08 15:52
+ *
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("hello world!2");
         System.out.println("hello world!3");
+        System.out.println("hello world!4");
     }
 }
