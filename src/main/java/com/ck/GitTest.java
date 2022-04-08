9ea6721d606331cccccf48adc0d11a63d95a8fa3
arbout <about1134@petalmail.com>
2022-04-08 15:55:21 +0800

    first commit

---
+++ src/main/java/com/ck/GitTest.java
@@ -0,0 +1,11 @@
+package com.ck;
+
+/**
+ * @author about1134
+ * @create 2022-04-08 15:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
